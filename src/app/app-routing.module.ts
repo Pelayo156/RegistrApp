6ab83114a5041bc6ddd4bd96fa4260ded9b2f6db
Pelayo156 <basti.molina1@gmail.com>
2024-09-04 20:59:59 -0400

    Agregando page de recuperar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'recuperar',
+    loadChildren: () => import('./pages/recuperar/recuperar.module').then( m => m.RecuperarPageModule)
+  },
 ];
 
 @NgModule({
