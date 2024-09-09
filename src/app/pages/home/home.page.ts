1d45e2746c2b8457b735cc3cf569c0e2fa62bb5c
AndreaSantanaCea1999 <165937920+AndreaSantanaCea1999@users.noreply.github.com>
2024-09-08 23:22:25 -0300

    modificacion de home y docente

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -3,19 +3,27 @@ import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
   username: string;
+  courses: Array<{ name: string, attendancePercentage: number }>;
 
   constructor(private route: ActivatedRoute) {
     this.username = '';
+    this.courses = [
+      
+      { name: 'Arquitectura', attendancePercentage: 85 },
+      { name: 'Programacion de aplicaciones moviles', attendancePercentage: 90 },
+      { name: 'Estadistica descriptiva', attendancePercentage: 80 },
+      { name: 'Calidad de software', attendancePercentage: 88 },
+    ];
   }
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
-      this.username = params['username'];
+      this.username = params['username'] || 'Usuario';
       console.log(this.username);
     });
   }
