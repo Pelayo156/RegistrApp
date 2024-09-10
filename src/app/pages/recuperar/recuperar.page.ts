eabd5f318aa32a3c1bbec9c87464773048b06f3c
joselyn <165937290+iosiWuaja@users.noreply.github.com>
2024-09-10 00:05:12 -0300

    función añadida al botón enviar

--- src/app/pages/recuperar/recuperar.page.ts
+++ src/app/pages/recuperar/recuperar.page.ts
@@ -7,10 +7,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./recuperar.page.scss'],
 })
 export class RecuperarPage implements OnInit {
+  email: string = '';
+  alertButtons = ['Salir'];
 
   constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  emailValido() : boolean{
+    const patronEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return patronEmail.test(this.email); 
+  }
 }
