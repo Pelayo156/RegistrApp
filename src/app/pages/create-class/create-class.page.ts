93e5215f9e8d04b046c83ef9c68f983ad5668bd3
AndreaSantanaCea1999 <165937920+AndreaSantanaCea1999@users.noreply.github.com>
2024-11-14 16:09:56 -0300

    Formulario crear clase

--- src/app/pages/create-class/create-class.page.ts
+++ src/app/pages/create-class/create-class.page.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-create-class',
   templateUrl: './create-class.page.html',
   styleUrls: ['./create-class.page.scss'],
 })
-export class CreateClassPage implements OnInit {
+export class CreateClassPage {
+
+
+  fechaInicio: string = '';
+  horaInicio: string = '';
+  horaTermino: string = '';
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
+  guardarClase() {
+    if (this.fechaInicio && this.horaInicio && this.horaTermino) {
+ 
+      const fechaHoraInicio = `${this.fechaInicio} ${this.horaInicio}`;
+      const fechaHoraTermino = `${this.fechaInicio} ${this.horaTermino}`;
+
+      console.log('Clase guardada:');
+      console.log('Fecha y Hora de Inicio:', fechaHoraInicio);
+      console.log('Fecha y Hora de Término:', fechaHoraTermino);
+
+      
+    } else {
+      console.error('Por favor, ingresa todas las fechas y horas.');
+    }
+  }
 }
